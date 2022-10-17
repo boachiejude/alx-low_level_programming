3b1811c45a8c5b3bdfff8c32a191627505149afd
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-17 09:41:01 +0000

    Added files via upload

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,3 @@ int main(void) {
     printf("Size of a long long int: %lu byte(s) \n", sizeof(long long int));
     printf("Size of a float: %lu byte(s) \n", sizeof(float));
 }
-
-
-// Size of a char: 1 byte(s)
-// Size of an int: 4 byte(s)
-// Size of a long int: 4 byte(s)
-// Size of a long long int: 8 byte(s)
-// Size of a float: 4 byte(s)
