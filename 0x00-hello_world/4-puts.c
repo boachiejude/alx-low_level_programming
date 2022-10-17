ceef3a943166b44d8437725f19c362a21f3152d0
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-17 09:53:22 +0000

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Hello, World!");
+/**
+* main - Entry point of the program
+* description - using puts to print a string to stdout
+* return - 0
+*/ 
+
+int main(void)
+{
+
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+* main - Entry point of the program
+* description - using printf to print a string to stdout
+* return - 0
+*/ 
+
+int main(void)
+{
     printf("Hello, World!");
     return 0;
 }
