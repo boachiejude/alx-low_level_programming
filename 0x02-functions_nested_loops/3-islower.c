613240cac0048e55b1290e47f15f257aeef77520
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-19 11:48:04 +0000

    Betty dey vex herh!

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * islower: checks if a character is in lowercase
+ * Description: function
+ * Return: 0
+**/
+int _islower(int c)
+{
+    if (islower(c))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
