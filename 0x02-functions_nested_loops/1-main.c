28f1bd7deec28fe84e2bb7207cc189b439003db5
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-19 10:48:21 +0000

    Finally figured out what Betty wants

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include "1-alphabet.c"
+
 /**
  * main - check the code
  * Description: description
  * Return: Always 0.
- */
+**/
 int main(void)
 {
-print_alphabet();
-return (0);
+	print_alphabet();
+	return (0);
 }
