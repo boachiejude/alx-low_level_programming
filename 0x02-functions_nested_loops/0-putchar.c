e838d5b0962d2450816c7c5049c33b0c8da21e0d
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-19 10:16:28 +0000

    Betty!

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main: entry function
+ * Description: prints "_putchar"
+ * Return: 0 - success value
+**/
 int main(void)
 {
-    printf("_putchar\n");
-
-    return (0);
+printf("_putchar\n");
+return (0);
 }
