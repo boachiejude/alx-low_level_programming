66f24ab21fd25408d0f63c79ed3c521894ebf1c0
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-20 08:53:25 +0000

    isupper task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * main: entry point
+ * Desription: main function
+ * Return: 0
+**/
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
