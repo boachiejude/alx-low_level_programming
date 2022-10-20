8c80a278cdce40a8003d4ec7880a603db0dd85b0
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-20 11:55:30 +0000

    stupid mistake lol

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - causes an infinite loop
- * Description: description
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	/* int i;*/
+        int i;
 
-	printf("Infinite loop incoming :(\n");
+        i = 98 % 2;
+        positive_or_negative(i);
 
-	/* i = 98 % 2;*/
-
-	/**
-	 * while (i < 10)
-	 * {
-	 *	putchar(i);
-	 * }
-	 */
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
+        return (0);
 }
