103b5a6f04dc84eee048430c2188d5f435b1c61b
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-20 07:56:17 +0000

    debugging Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,13 +5,13 @@
  * Description: -
  * Return: 0
 **/
-
 int main(void)
 {
-int i;
-i = 98;
+	int i;
+	
+	i = 98;
 
-positive_or_negative(i);
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
