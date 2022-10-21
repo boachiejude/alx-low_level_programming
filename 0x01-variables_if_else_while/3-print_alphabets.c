81d9005fe1ff87238f9e013eca085655cd6e958f
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-21 14:45:35 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints alphabets
+ * Return: 0 - successful
+**/
+
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+
+	return (0);
+}
