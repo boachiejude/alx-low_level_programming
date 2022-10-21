d50724f1d0210492adec50db5ab74e73edcca96a
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-21 15:00:46 +0000

    tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints alphabets
+ * Return: 0 - successful
+**/
+
+int main(void)
+{
+	printf("zyxwvutsrqponmlkjihgfedcba\n");
+
+	return (0);
+}
