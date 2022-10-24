21a277c7e37fdf7b0b88c16a42f6df2856e5f243
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 12:04:08 +0000

    solution: 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse
+ * puts2 - prints a string in reverse
  *
  * @s: string parameter input
  *
@@ -15,15 +15,7 @@ void puts2(char *s)
 
 	/*finds the length of string without null character*/
 	for (index = 0; s[index] != '\0'; ++index)
-		;
-
-	/*print char from the last index as you decrement*/
-	for (--index; index >= 0; --index)
 	{
-		if (index % 2 == 0)
-		{
-			continue;
-		}
 		putchar(s[index]);
 	}
 	putchar('\n');
