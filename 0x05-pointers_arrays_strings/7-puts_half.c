31291f34491cc6e7340c341774b38eff67671d7d
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 12:18:28 +0000

    solution: 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void puts_half(char *s)
+{
+	int index;
+	int length = strlen(s);
+
+	/*finds the length of string without null character*/
+	for (index = 0; index < (length / 2); ++index)
+	{
+		putchar(s[index]);
+	}
+	putchar('\n');
+}
