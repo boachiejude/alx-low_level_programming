111ae3ac5c3af22399ec33a3add3f7b4376046ab
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 09:30:14 +0000

    First attempt: python-style swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap - function
+ * @a: pointer to an integer variable a
+ * @b: pointer to an integer variable b
+ * Return: void
+**/
+
+void swap(int *a, int *b)
+{
+	*a, *b = *b, *a;
+}
