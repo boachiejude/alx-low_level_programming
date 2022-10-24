85c7cd01f647239ab038dd29c8739c19434c37c2
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 10:21:02 +0000

    Attempt 1: for loop

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be reversed and printed
+ * Return: void
+**/
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+
+	for (int i = 0;i < length; i++)
+	{
+		printf("%s", s[length - i]);
+	};
+	printf("\n");
+}
