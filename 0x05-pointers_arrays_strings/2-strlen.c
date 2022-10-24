1288aca69d821843b3814770c013d6c73e158543
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 09:48:11 +0000

    First attemp: strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - function that prints the length of a string
+ * @s: char variable
+ * Return: void
+**/
+
+void _strlen(char *s)
+{
+	printf(strlen(s));
+}
