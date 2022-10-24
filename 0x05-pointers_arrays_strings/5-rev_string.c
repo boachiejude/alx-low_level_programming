0420db6d3520108fbcf9b3ba1eadd28cf495229f
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 11:20:55 +0000

    coding

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,25 @@
-#include "main.h"
+#include"main.h"
 #include <stdio.h>
 #include <string.h>
 
 /**
  * rev_string - prints a string in reverse
- * @s: string to be reversed and printed
- * Return: void
-**/
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
 
 void rev_string(char *s)
 {
-	s = strrev(s);
+	int index;
+	int *a = *s;
+	char b;
+	*a = b;
+	int len = strlen(s);
+
+	for (index = 0;index < len; index--)
+	{
+		s[index] = a[len - index];
+	}
 }
