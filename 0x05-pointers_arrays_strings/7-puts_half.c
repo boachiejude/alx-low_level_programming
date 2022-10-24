c753d1e9d263a5fd2ec6d088f0f70164fc2c5ded
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 15:15:54 +0000

    sigh...Changed printf to putchar

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * puts_half - prints half of a string
@@ -19,11 +18,11 @@ void puts_half(char *str)
 	if (l % 2 == 0)
 	{
 		for (n = l / 2; str[n] != '\0'; ++n)
-			printf("%d", (str[n]));
+			putchar(str[n]);
 	} else
 	{
 		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
-			printf("%d",(str[n]));
+			putchar(str[n]);
 	}
-	printf("\n");
+	putchar('\n');
 }
