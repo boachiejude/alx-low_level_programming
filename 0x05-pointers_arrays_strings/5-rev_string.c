fda65d46e25e04376b3a387631ae56bf8fa8e71e
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-24 10:44:12 +0000

    skipped task 4:task 5, attempt 1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: string to be reversed and printed
+ * Return: void
+**/
+
+void rev_string(char *s)
+{
+	return (strrev(s));
+}
