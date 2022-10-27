123c4a26bf8ae924183c1badab109b3b1b7ac76f
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-27 09:46:11 +0000

    requirement fail...

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,31 @@
-#include "main.h"
-#include <string.h>
-
 /**
- * _strncat - func
- * @dest: destination string
- * @src: source string
- * @n: n bytes to use
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
  * Return: @dest
-**/
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char a = strncat(dest, src, n);
+	int c, i;
 
-	return (*dest);
+	c = 0;
+
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
 }
