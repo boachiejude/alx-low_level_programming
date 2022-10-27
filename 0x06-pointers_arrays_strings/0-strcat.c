d0c0e4a4c294baabe27d5c25cbebeec0769b4951
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-10-27 09:15:28 +0000

    Attempt 1: strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - func
+ * @dest: destination string
+ * @src: source string
+ * Return: char
+**/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
