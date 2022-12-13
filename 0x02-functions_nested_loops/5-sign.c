faccdd471564b9378e09a0f4d738696bb48717f3
Jude Boachie <judeboachiekwame@gmail.com>
2022-12-13 16:51:08 +0000

    Correct code logic

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <ctype.h>
+#include <stdio.h>
 
 /**
  * print_sign - function
@@ -10,8 +11,11 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	    return ("+, 1");
+		printf("+");
+	    return (1);
     if (n < 0)
-	    return ("-, -1");
-    return ("0, 0");
+		printf("-");
+	    return (-1);
+	printf("0");
+    return (0);
 }
