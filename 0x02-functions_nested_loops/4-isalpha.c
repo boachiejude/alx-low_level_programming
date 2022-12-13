4e4ddb9a67a3afebe1839e22952ae81baa90ed57
Jude Boachie <judeboachiekwame@gmail.com>
2022-12-13 16:31:42 +0000

    Fix betty docs error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <ctype.h>
-
+/**
+ * _isalpha: checks if a function is a letter
+ * Description: function
+ * @c: input for an integer value
+ * Returns: 1 if the input is a letter
+*/
 int _isalpha(int c)
 {
 	if (islower(c) || isupper(c))
