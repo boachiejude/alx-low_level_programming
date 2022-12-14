39a9214298dfc271d5ee02eacc9243b81c885053
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-12-14 12:13:54 +0000

    apply Likem's help

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _abs - func
+ * Description: gets the abs value
+ * @n: integer input
+ * Return: abs value of n
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
+/**
  * print_last_digit - func
  * Description: returns the last digit of a number
  * @n: number
@@ -9,14 +26,6 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n *= -1;
-	}
-	if (n == 0)
-	{
-		return (00);
-	};
-	_putchar('0' + (n % 10));
-	return ((n % 10));
+	_putchar('0' + (_abs(n % 10)));
+	return (_abs(n % 10));
 }
