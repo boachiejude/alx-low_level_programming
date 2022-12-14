bb994e51b1d062bb6c4f76ea0643e39d2ef7912d
Jude Boachie <87368331+boachiejude@users.noreply.github.com>
2022-12-14 10:41:02 +0000

    Fix code logic

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,29 @@
 void print_to_98(int n)
 {
 	int i;
-	
-	for (i = n; i <= 98; i++)
+
+	if (i >= 98)
 	{
-		printf("%d", i);
-		if (i != 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf(", ");
-		};
-	}
-	printf("\n");
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			};
+		}
+		printf("\n");
+	};
+	if (i < 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			};
+		}
+		printf("\n");
+	};
 }
