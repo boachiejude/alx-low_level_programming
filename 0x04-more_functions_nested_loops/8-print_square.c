ec8f21f2dfd3f689608f7caaeab0cc0ad7481198
Jude Boachie <judeboachiekwame@gmail.com>
2022-12-15 23:18:36 +0000

    Attempt fixing code output

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,15 @@
 
 void print_square(int size)
 {
-	for (int i = 0; i < size; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
 	{
-		for (int j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			printf("#");
 		};
+		printf("\n");
 	};
 };
