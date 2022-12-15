e506b1b036760e281cd46f704742444d3dc80047
Jude Boachie <judeboachiekwame@gmail.com>
2022-12-15 23:31:26 +0000

    Apply help from iAmG-r00t

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,32 +1,26 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_diagonal - print a diagonal line
  *
  * @n: is the number of times the \ character
  *     should be printed
- * Return: none
 */
 
 void print_diagonal(int n)
 {
+	int postn, space;
+
 	if (n <= 0)
-	{
-		printf("\n");
-	}
+		_putchar('\n');
 	else
 	{
-		int i;
-
-		for (i = 0; i < n; i++)
+		for (postn = 1; postn <= n; ++postn)
 		{
-			while (i > 0)
-			{
-				printf(" ");
-				i--;
-			}
-			printf("\\\n");
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
 		}
 	}
 }
-
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,21 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_square - print a square using the character #
  *
  * @size: is the size of the square
  *
- * Return: none
+ * Return: Always 0 (Success)
 */
 
 void print_square(int size)
 {
-	int i;
+	int row, column;
 
-	for (i = 0; i < size; i++)
+	for (row = 1; row <= size; ++row)
 	{
-		int j;
-
-		for (j = 0; j < size; j++)
-		{
-			_putchar(35);
-		};
-	};
-};
+		for (column = 1; column <= size; ++column)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
