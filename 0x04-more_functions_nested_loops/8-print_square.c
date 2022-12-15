80a4af607b9e6ff94fd4e9d0a0205a0332e548e8
Jude Boachie <judeboachiekwame@gmail.com>
2022-12-15 23:21:05 +0000

    Attempt fixing requirement errors

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_square - print a square using the character #
  *
  * @size: is the size of the square
  *
- * Return: Always 0 (Success)
+ * Return: none
 */
 
 void print_square(int size)
