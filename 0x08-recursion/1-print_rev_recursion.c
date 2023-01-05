173bf5f63f14924ac6d5e5bce1add3a96790bbcd
Jude Boachie <judeboachiekwame@gmail.com>
2023-01-05 22:42:32 +0000

    upload print_rev_recursion solution

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string with recursion
+ * @s: the string to print
+ * Return: None
+**/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		printf("%c", *s);
+	}
+	else
+	printf("\n");
+}
