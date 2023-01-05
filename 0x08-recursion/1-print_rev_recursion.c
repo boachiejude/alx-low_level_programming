96f4e9846762f11d67f9b80f6271c788a71a8ca2
Jude Boachie <judeboachiekwame@gmail.com>
2023-01-05 23:08:10 +0000

    debug .

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,13 @@
 **/
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == 0)
+	{
+		printf("\n");
+	}
+	else
 	{
 		_print_rev_recursion(s + 1);
 		printf("%c", *s);
 	}
-	else
-	printf("\n");
-	return;
 }
