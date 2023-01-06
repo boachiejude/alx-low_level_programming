53305f32d24f904a77da37c7a9faadaecf1cc725
Jude Boachie <judeboachiekwame@gmail.com>
2023-01-06 01:58:43 +0000

    attempt: fix code logic

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,23 @@
-int abs(int n)
-{
-	if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
-}
-
 /**
  * _sqrt_recursion - uses recursion to determine the square root of a number
  * @n: number
- * @i: current guess of the square root
- * @p: precision
+ * @i: guess
  * Return: sqrt of n
 **/
-int _sqrt_recursion(int n, int i, int p) {
-	int nextGuess;
-
-   if (abs(i * i - n) < p) {
-	  return i;
-   }
-
-   nextGuess = (n / i + i) / 2;
-
-   return _sqrt_recursion(n, nextGuess, p);
+int _sqrt_recursion(int n, int i) {
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		if (i)
+		{
+			return (_sqrt_recursion(n, i - 1));
+		}
+		else
+		{
+			i = n;
+		}
+	}
 }
