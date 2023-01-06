f276459e3a8288396565eb1565cb5c5db34d44e4
Jude Boachie <judeboachiekwame@gmail.com>
2023-01-06 01:31:21 +0000

    attempt: sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _sqrt_recursion - uses recursion to determine the square root of a number
+ * @n: number
+ * @i: current guess of the square root
+ * Return: sqrt of n
+**/
+int _sqrt_recursion(int n, int i = 0)
+{
+	if (i * i == n)
+	{
+		return i;
+	}
+	if ((i * i) > n)
+	{
+		return i - 1;
+	}
+	return _sqrt_recursion(n, i + 1);
+}
