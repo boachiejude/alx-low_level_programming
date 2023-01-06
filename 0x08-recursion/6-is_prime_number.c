84635ffdb0f16f3a6f385d427dfa24fde3c763dc
Jude Boachie <judeboachiekwame@gmail.com>
2023-01-06 02:21:21 +0000

    attempt 0.2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,18 +4,19 @@
  * @i: indexer
  * Returns: boolean indicating whether the number is prime or not
 */
-int is_prime_number(int n, int i) {
+int is_prime_number(int n, int i)
+{
 	if (n < 2)
 	{
-		return 0;
+		return (0);
 	}
 	if (i == 1)
 	{
-		return 1;
+		return (1);
 	}
 	if (n % i == 0)
 	{
-		return 0;
+		return (0);
 	}
-	return is_prime_number(n, i - 1);
+	return (is_prime_number(n, i - 1));
 }
